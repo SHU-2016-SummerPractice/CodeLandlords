77d200bd911f568ebccf01a6be211961d374cbdb
Jason Song <i@wolfogre.com>
2016-08-12 21:28:38 +0800

    New way

--- src/com/wolfogre/codelandlords/WikipediaCardsChecker.java
+++ src/com/wolfogre/codelandlords/WikipediaCardsChecker.java
@@ -36,83 +36,53 @@ public class WikipediaCardsChecker implements CardsChecker {
 
     public CardsType getCardsType(String cards){
         //TODO:判断牌型的逻辑
+        if(cards.isEmpty())
+            return CardsType.错误;
         for(char ch : cards.toCharArray())
             if(FormatCards.getIndexByCard(ch) == -1)
                 return CardsType.错误;
         FormatCards formatCards = new FormatCards(cards);
-        switch (cards.length()){
+        switch (formatCards.getCounts()[0]){
             case 1:
-                return CardsType.单张;
-            case 2:
-                if(formatCards.size() == 1)
-                    return CardsType.一对;
-                if(cards.charAt(0) == 'S' && cards.charAt(1) == 'M')
+                if(cards.length() == 1)
+                    return CardsType.单张;
+                if(cards.length() >= 5
+                        && formatCards.getCards()[formatCards.size() - 1] <= 'A'
+                        && formatCards.isContinuous(0, formatCards.size()))
+                    return CardsType.单顺;
+                if(cards.length() == 2
+                        && formatCards.getCards()[0] == 'M'
+                        && formatCards.getCards()[1] == 'S')
                     return CardsType.火箭;
                 return CardsType.错误;
+            case 2:
+                if(cards.length() == 2)
+                    return CardsType.一对;
+                if(cards.length() >= 3
+                        && formatCards.getCounts()[0] == 2
+                        && formatCards.getCounts()[formatCards.size() - 1] == 2
+                        && formatCards.getCards()[formatCards.size() - 1] <= 'A')
+                    return CardsType.双顺;
             case 3:
-                if(formatCards.size() == 1)
+                if(cards.length() == 3)
                     return CardsType.三不带;
-                return CardsType.错误;
-            case 4:
-                if(formatCards.size() == 1)
-                    return CardsType.炸弹;
-                if(formatCards.size() == 2 && formatCards.getCounts()[0] == 3)
+                if(cards.length() == 4)
                     return CardsType.三带一;
-                return CardsType.错误;
-            case 5:
-                if(formatCards.size() == 2 && formatCards.getCounts()[0] == 3)
+                if(cards.length() == 5 && formatCards.size() == 2)
                     return CardsType.三带二;
-                if(formatCards.isContinuous(0, formatCards.size())
-                        && FormatCards.getIndexByCard(formatCards.getCards()[cards.length() - 1]) <= FormatCards.getIndexByCard('A'))
-                    return CardsType.单顺;
-                return CardsType.错误;
-            case 6: //TODO
-                if(cards.charAt(0) == cards.charAt(1) && cards.charAt(1) == cards.charAt(2) && cards.charAt(2) == cards.charAt(3)
-                        || cards.charAt(1) == cards.charAt(2) && cards.charAt(2) == cards.charAt(3) && cards.charAt(3) == cards.charAt(4)
-                        || cards.charAt(2) == cards.charAt(3) && cards.charAt(3) == cards.charAt(4) && cards.charAt(4) == cards.charAt(5))
-                    return CardsType.四带二;
-                if(formatCards.isContinuous(0, formatCards.size())
-                        && FormatCards.getIndexByCard(formatCards.getCards()[cards.length() - 1]) <= FormatCards.getIndexByCard('A'))
-                    return CardsType.单顺;
-                return CardsType.错误;
-            case 7: //TODO
-                if(formatCards.isContinuous(0, formatCards.size())
-                        && FormatCards.getIndexByCard(formatCards.getCards()[cards.length() - 1]) <= FormatCards.getIndexByCard('A'))
-                    return CardsType.单顺;
-                return CardsType.错误;
-            case 8: //TODO
-                if(formatCards.isContinuous(0, formatCards.size())
-                        && FormatCards.getIndexByCard(formatCards.getCards()[cards.length() - 1]) <= FormatCards.getIndexByCard('A'))
-                    return CardsType.单顺;
-                return CardsType.错误;
-            case 9: //TODO
-                if(formatCards.isContinuous(0, formatCards.size())
-                        && FormatCards.getIndexByCard(formatCards.getCards()[cards.length() - 1]) <= FormatCards.getIndexByCard('A'))
-                    return CardsType.单顺;
-                return CardsType.错误;
-            case 10: //TODO
-                if(formatCards.isContinuous(0, formatCards.size())
-                        && FormatCards.getIndexByCard(formatCards.getCards()[cards.length() - 1]) <= FormatCards.getIndexByCard('A'))
-                    return CardsType.单顺;
-                return CardsType.错误;
-            case 11: //TODO
-                if(formatCards.isContinuous(0, formatCards.size())
-                        && FormatCards.getIndexByCard(formatCards.getCards()[cards.length() - 1]) <= FormatCards.getIndexByCard('A'))
-                    return CardsType.单顺;
-                return CardsType.错误;
-            case 12: //TODO
-                if(formatCards.isContinuous(0, formatCards.size())
-                        && FormatCards.getIndexByCard(formatCards.getCards()[cards.length() - 1]) <= FormatCards.getIndexByCard('A'))
-                    return CardsType.单顺;
-                return CardsType.错误;
-            case 13: //TODO
-            case 14: //TODO
-            case 15: //TODO
-            case 16: //TODO
-            case 17: //TODO
-            case 18: //TODO
-            case 19: //TODO
-            case 20: //TODO
+                int endOfThree;
+                endOfThree = 0;
+                while (formatCards.getCounts()[endOfThree] != 3 && endOfThree < formatCards.size()) {
+                    ++endOfThree;
+                }
+                //TODO:注意 333 444 555 QQQ
+                if(formatCards.isContinuous(0, endOfThree)){
+                    if(formatCards.size() == endOfThree)
+                        return CardsType.飞机;
+                    //TODO:碰到问题了，333 444 555 666 JJJJ算不算？
+                }
+            case 4:
+                //TODO:注意 JJJJ 333 444 555 666
             default:
                 return null;
         }
