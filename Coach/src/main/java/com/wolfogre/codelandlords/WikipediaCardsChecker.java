2010227b05a42d9662be526941e86f7fd2cf573d
qjkobe <513967622@qq.com>
2016-08-20 15:01:29 +0800

    完成了牌型检测功能

--- Coach/src/main/java/com/wolfogre/codelandlords/WikipediaCardsChecker.java
+++ Coach/src/main/java/com/wolfogre/codelandlords/WikipediaCardsChecker.java
@@ -27,7 +27,9 @@ public class WikipediaCardsChecker implements CardsChecker {
         双顺,
         四带二,
         飞机,
+        飞机带翼,
         航天飞机,
+        航天飞机带翼,
         炸弹,
         火箭,
     }
@@ -72,19 +74,66 @@ public class WikipediaCardsChecker implements CardsChecker {
                     return CardsType.三带一;
                 if(cards.length() == 5 && formatCards.size() == 2)
                     return CardsType.三带二;
-                int endOfThree;
-                endOfThree = 0;
-                while (formatCards.getCounts()[endOfThree] != 3 && endOfThree < formatCards.size()) {
-                    ++endOfThree;
+                if(cards.length() == 5 && formatCards.size() == 3){
+                    return CardsType.错误;
                 }
-                //TODO:注意 333 444 555 QQQ
-                if(formatCards.isContinuous(0, endOfThree)){
-                    if(formatCards.size() == endOfThree)
-                        return CardsType.飞机;
-                    //TODO:碰到问题了，333 444 555 666 JJJJ算不算？
+                if(cards.length() > 5
+                        && judgeStraight(formatCards, 0, 3)){
+                    return CardsType.飞机;
                 }
+                if(cards.length() > 7
+                        && judgePlaneWithWing(formatCards, 0, 3, getPlaneSize(formatCards, 3))){
+                    return CardsType.飞机带翼;
+                }
+//                int endOfThree;
+//                endOfThree = 0;
+//                while (formatCards.getCounts()[endOfThree] != 3 && endOfThree < formatCards.size()) {
+//                    ++endOfThree;
+//                }
+//                //注意 333 444 555 QQQ
+//                if(formatCards.isContinuous(0, endOfThree)){
+//                    if(formatCards.size() == endOfThree)
+//                        return CardsType.飞机;
+//                    //碰到问题了，333 444 555 666 JJJJ算不算？
+//                }
             case 4:
-                //TODO:注意 JJJJ 333 444 555 666
+                if(formatCards.size() == 1){
+                    return CardsType.炸弹;
+                }
+                if(judgeStraight(formatCards, 0, 4)){
+                    return CardsType.航天飞机;
+                }
+                if(judgePlaneWithWing(formatCards, 0, 4, getPlaneSize(formatCards, 4))){
+                    return CardsType.航天飞机带翼;
+                }
+                if(formatCards.size() == 3
+                        && formatCards.getCounts()[1] == formatCards.getCounts()[2]
+                        && formatCards.getCounts()[1] <= 2){
+                    //TODO:比如44448888，算是4带二，但是是四个8带两个4，而不是四个4带两个8
+                    return CardsType.四带二;
+                }
+                if(cards.length() == 5
+                        || cards.length() == 7){
+                    return CardsType.错误;
+                }
+                return CardsType.错误;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                if(formatCards.size() == 1){
+                   return CardsType.炸弹;
+                }
+                if(formatCards.size() == 3
+                        && formatCards.getCounts()[1] == formatCards.getCounts()[2]
+                        && formatCards.getCounts()[1] <= 2){
+                    return CardsType.四带二;
+                }
+                if(formatCards.size() >= 4){
+                    //TODO:类似与5555566666带翅膀的，我就不考虑了。
+                    return CardsType.错误;
+                }
+                return CardsType.错误;
             default:
                 return null;
         }
@@ -137,13 +186,6 @@ public class WikipediaCardsChecker implements CardsChecker {
         return false;
     }
 
-    @Test
-    public void testJSP(){
-        String cards = "2233";
-        FormatCards formatCards = new FormatCards(cards);
-        System.out.println(judgeStraightPair(formatCards, 0));
-    }
-
     /**
      * 判断是否成顺子的通用方法，在num为3的时候请注意，这里只能判断3是否成顺，如果带了对子，请判断对子的数量和仨子的数量是否相等
      * @param formatCards 牌对象
@@ -166,12 +208,59 @@ public class WikipediaCardsChecker implements CardsChecker {
         return false;
     }
 
-    @Test
-    public void testJS(){
-        String cards = "333444555";
-        FormatCards formatCards = new FormatCards(cards);
-        System.out.println(judgeStraight(formatCards, 0, 3));
+    /**
+     * 判断是否成飞机带翅膀
+     * @param formatCards 牌对象
+     * @param index 从第几类数字开始判断
+     * @param num 三顺3，航天飞机4
+     * @param planeSize 飞机数量。333444555，飞机数量3
+     * @return
+     */
+    private Boolean judgePlaneWithWing(FormatCards formatCards, int index, int num, int planeSize){
+        if(index == planeSize){
+            //判断后面是对子或者别的
+            if(formatCards.size() == (planeSize << 1)){
+                for(int i = formatCards.size()-1; i > planeSize; i--){
+                    if(formatCards.getCounts()[i] != formatCards.getCounts()[i-1]){
+                        return false;
+                    }
+                }
+                return true;
+            }else{
+                return false;
+            }
+        }
+        if(formatCards.getCounts()[index] == num){
+            if(index + 1 < planeSize
+                    && (FormatCards.getIndexByCard(formatCards.getCards()[index]) != FormatCards.getIndexByCard(formatCards.getCards()[index+1]) - 1
+                    || formatCards.getCards()[index+1] == 'A') ){
+                return false;
+            }
+            return judgePlaneWithWing(formatCards, index+1, num, planeSize);
+        }
+        return false;
     }
+
+    @Test
+    public void TestJPWW(){
+        String cards = "3333444478";
+        FormatCards formatCards = new FormatCards(cards);
+        System.out.println(judgePlaneWithWing(formatCards, 0, 4, 2));
+    }
+
+    /**获取同一长度牌的的数量。比如333444，返回2。注意这里不会报错，请先判断是否连续
+     * @param formatCards 牌对象
+     * @param num 三顺3
+     * @return
+     */
+    private int getPlaneSize(FormatCards formatCards, int num){
+        int i = 0, count = 0;
+        while(formatCards.getCounts()[i++] == num){
+            count ++;
+        }
+        return count;
+    }
+
 }
 
 
